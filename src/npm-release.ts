0afd91239fbb0f8632beb5647dc21139a0f4b393
FreePhoenix888 <freephoenix888@gmail.com>
2023-05-30 17:52:37 +0600

    Use right version in deep.json

--- src/npm-release.ts
+++ src/npm-release.ts
@@ -1,9 +1,13 @@
-import exec from "@simplyhexagonal/exec";
-import { execWrapped } from "./exec-wrapped";
-import { updateDeepJsonVersion } from "./update-version-in-json-object";
+import exec from '@simplyhexagonal/exec';
+import { execWrapped } from './exec-wrapped';
+import { updateDeepJsonVersion } from './update-version-in-json-object';
 
-export async function npmRelease({deepJsonFilePath,newVersion,packageJsonPath}: NpmReleaseParam) {
-   const packageJson = await import(packageJsonPath);
+export async function npmRelease({
+  deepJsonFilePath,
+  newVersion,
+  packageJsonPath,
+}: NpmReleaseParam) {
+  const packageJson = await import(packageJsonPath);
   const { execPromise } = exec(`npm view ${packageJson.name} version`);
   const execResult = await execPromise;
   if (execResult.exitCode !== 0) {
@@ -16,26 +20,21 @@ export async function npmRelease({deepJsonFilePath,newVersion,packageJsonPath}: 
       `Version ${packageJson.version} in ${packageJsonPath} is outdated. Latest version in npm is ${npmLatestPackageJsonVersion}. Execute npm-pull`
     );
   }
-  if (npmLatestPackageJsonVersion === packageJsonVersion) {
-    await execWrapped({
+  if (npmLatestPackageJsonVersion <= packageJsonVersion) {
+    const {
+      execResult: { stdoutOutput: npmVersionStdoutOutput },
+    } = await execWrapped({
       command: `npm version --allow-same-version ${newVersion}`,
     });
     await updateDeepJsonVersion({
-      version: newVersion,
-      filePath: deepJsonFilePath,
-    });
-  }
-  if (npmLatestPackageJsonVersion < packageJsonVersion) {
-    await execWrapped({ command: `npm version ${packageJsonVersion}` });
-    await updateDeepJsonVersion({
-      version: newVersion,
+      version: npmVersionStdoutOutput,
       filePath: deepJsonFilePath,
     });
   }
 }
 
 export interface NpmReleaseParam {
-   newVersion: string;
-   packageJsonPath: string;
-   deepJsonFilePath: string;
+  newVersion: string;
+  packageJsonPath: string;
+  deepJsonFilePath: string;
 }
