ee9d79b40310bdcde56138bfca840bdc3375f988
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-10 21:21:49 +0600

    Add npm install

---
+++ src/npm-install.ts
@@ -0,0 +1,46 @@
+import { SemVer } from 'semver';
+import semver from 'semver';
+import { execAndLogStdoutOrThrowError } from './exec-and-log-stdout-or-throw-error';
+import { DeepJson } from './deep-json';
+import { type PackageJson } from 'types-package-json';
+
+/**
+ * Installs a package
+ *
+ * @throws {@link Error} if the version is invalid
+ * @throws {@link Error} if the exit code of npm install is not 0
+ */
+export async function npmInstall(param: NpmInstallParam) {
+  const { packageName, version, deepJsonFilePath, packageJsonFilePath } = param;
+  if (version && !semver.valid(version))
+    throw new Error(`Invalid version ${version}`);
+  let npmInstallCommand = `npm install ${packageName}`;
+  if (version) {
+    npmInstallCommand += `@${version}`;
+  }
+  await execAndLogStdoutOrThrowError({
+    command: npmInstallCommand,
+  });
+
+  const { default: packageJson }: { default: Partial<PackageJson> } =
+    await import(packageJsonFilePath, { assert: { type: 'json' } });
+  const packageJsonDependencyVersion = packageJson.dependencies![packageName];
+
+  const { default: deepJson }: { default: DeepJson } = await import(
+    deepJsonFilePath,
+    { assert: { type: 'json' } }
+  );
+
+  const deepJsonDependencyIndex = deepJson.dependencies.findIndex(
+    (dependency) => dependency.name === packageName
+  );
+  deepJson.dependencies[deepJsonDependencyIndex].version =
+    packageJsonDependencyVersion;
+}
+
+export interface NpmInstallParam {
+  packageName: string;
+  packageJsonFilePath: string;
+  deepJsonFilePath: string;
+  version: string | undefined;
+}
