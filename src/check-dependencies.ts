114beb01557b8b40a34aaa7749b88fe2343bcd71
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-10 20:06:52 +0600

    Sync versions

--- src/check-dependencies.ts
+++ src/check-dependencies.ts
@@ -1,27 +1,52 @@
 import fsExtra from 'fs-extra';
+import { DeepJson, DeepJsonDependency } from './deep-json';
+import semver from 'semver'
+import { type PackageJson } from 'types-package-json';
+
 
 export interface CheckDependenciesParam {
   deepJsonFilePath: string;
   packageJsonPath: string;
 }
 
-type Dependency = Record<string, string>;
-
 export async function syncDependencies({
   deepJsonFilePath,
   packageJsonPath,
 }: CheckDependenciesParam) {
-  const {default: deepJson} = await import(deepJsonFilePath, {assert: {type: 'json'}}) ;
-  const {default: packageJson} = await import(packageJsonPath, {assert: {type: 'json'}});
+  const {default: deepJson}: {default: DeepJson} = await import(deepJsonFilePath, {assert: {type: 'json'}}) ;
+  const {default: packageJson}: {default: Partial<PackageJson>} = await import(packageJsonPath, {assert: {type: 'json'}});
 
-  const missingDependenciesFromDeepJson: Array<Dependency> = deepJson.dependencies.filter((dependency: Dependency) => !!packageJson.dependencies[dependency.name]);
-  missingDependenciesFromDeepJson.forEach((dependency: Dependency) => {
-    const transformedDependency = {[dependency.name]: `~${dependency.version}`};
-    packageJson.dependencies = {...packageJson.dependencies, ...transformedDependency};
+  if(!packageJson.dependencies) {
+    packageJson.dependencies = {};
+  }
+
+  const missingDependenciesFromDeepJson: Array<DeepJsonDependency> = deepJson.dependencies.filter((dependency: DeepJsonDependency) => !!packageJson.dependencies![dependency.name]);
+  missingDependenciesFromDeepJson.forEach((dependency: DeepJsonDependency) => {
+    packageJson.dependencies = {...packageJson.dependencies, [dependency.name]: `~${dependency.version}`};
   })
 
   if (missingDependenciesFromDeepJson.length > 0) {
     await fsExtra.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
-    console.log(`${missingDependenciesFromDeepJson.map((dependency: Dependency) => dependency.name).join(', ')} added to package.json because they exist in deep.json`);
+    console.log(`${missingDependenciesFromDeepJson.map((dependency: DeepJsonDependency) => dependency.name).join(', ')} are added to package.json because they exist in deep.json`);
   }
+
+  deepJson.dependencies.forEach((dependency: DeepJsonDependency) => {
+    const isDeepJsonVersionGreater = semver.gt(dependency.version, packageJson.dependencies![dependency.name]);
+    if(isDeepJsonVersionGreater) {
+      packageJson.dependencies![dependency.name] = `~${dependency.version}`;
+    } else {
+      deepJson.dependencies = {...deepJson.dependencies, [dependency.name]: `~${dependency.version}`};
+    }
+  })
+
+  Object.entries(packageJson.dependencies).forEach(([dependencyName, dependencyVersion]) => { 
+    const deepJsonDependency = deepJson.dependencies.find(dependency => dependency.name === dependency.name);
+    if(!deepJsonDependency) return;
+    const isPackageJsonVersionGreater = semver.gt(dependencyVersion, deepJson.dependencies.find(dependency => dependency.name === dependency.name)!.version);
+    if(isPackageJsonVersionGreater) {
+      deepJson.dependencies = {...deepJson.dependencies, [dependencyName]: `~${dependencyVersion}`};
+    } else {
+      packageJson.dependencies![dependencyName] = `~${deepJsonDependency.version}`;
+    }
+  })
 }
