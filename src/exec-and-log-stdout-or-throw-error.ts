b768424343a3d3322d6336e528c9bd3f4e91cd21
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-10 21:54:59 +0600

    Destructure in fucntion body

--- src/exec-and-log-stdout-or-throw-error.ts
+++ src/exec-and-log-stdout-or-throw-error.ts
@@ -4,7 +4,8 @@ import exec from "@simplyhexagonal/exec";
  * Executes a command and logs stdout
  * @throws {@link Error} if the exit code is not 0
  */
-export async function execAndLogStdoutOrThrowError({ command }: { command: string }) {
+export async function execAndLogStdoutOrThrowError(param: { command: string }) {
+  const { command } = param;
    const { execPromise, execProcess } = exec(command);
    const execResult = await execPromise;
    if (execResult.exitCode !== 0) {
