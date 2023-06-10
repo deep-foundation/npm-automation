813e6089ab79facaf188efab9f158f9745c8d34c
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-10 21:21:59 +0600

    Rename execWrapper

--- src/exec-and-log-stdout-or-throw-error.ts
+++ src/exec-and-log-stdout-or-throw-error.ts
@@ -1,11 +1,16 @@
 import exec from "@simplyhexagonal/exec";
 
-export async function execWrapped({ command }: { command: string }) {
+/**
+ * Executes a command and logs stdout
+ * @throws {@link Error} if the exit code is not 0
+ * @returns {@link execAndLogStdoutOrThrowOnErrorResult}
+ */
+export async function execAndLogStdoutOrThrowError({ command }: { command: string }) {
    const { execPromise, execProcess } = exec(command);
    const execResult = await execPromise;
    if (execResult.exitCode !== 0) {
      throw new Error(execResult.stderrOutput);
    }
    console.log(execResult.stdoutOutput);
-   return { execProcess, execPromise, execResult };
+   return { execResult };
  }
