bd8f2f2213dc0da467bd65576937becb98bfa16f
FreePhoenix888 <freephoenix888@gmail.com>
2023-06-10 21:36:15 +0600

    Export npm install

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
+export { npmInstall,NpmInstallParam } from './npm-install';
 export { syncDependencies,SyncDependenciesParam } from './sync-dependencies';
 export { npmRelease, NpmReleaseParam } from './npm-release';
 export { npmPull, NpmPullParam } from './npm-pull';
-
