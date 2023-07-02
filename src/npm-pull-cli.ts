82ea31f774ac6cd4bf5fbe192c60a81c48fdace5
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-03 00:16:54 +0600

    Fix #8

--- src/npm-pull-cli.ts
+++ src/npm-pull-cli.ts
@@ -2,10 +2,16 @@ import exec from '@simplyhexagonal/exec';
 import { program } from 'commander';
 import path from 'path';
 import { npmPull } from './npm-pull.js';
+import  createDebugMessages from 'debug';
+import {fileURLToPath} from 'url'
+
 
 main();
 
 async function main() {
+  const debug = createDebugMessages(
+    '@deep-foundation/npm-automation:npm-pull-cli'
+  );
   program
     .name('npm-pull')
     .description('Pull latest version of a package from npm')
@@ -21,9 +27,14 @@ async function main() {
   program.parse(process.argv);
 
   let options = program.opts();
-  const packageName = options.packageName ?? await import(path.resolve('package.json'), {assert: {type: 'json'}}).then(
+  const filename = fileURLToPath(import.meta.url);
+  const dirname = path.dirname(filename);
+  const packageJsonFilePath = path.resolve(dirname,'package.json');
+  debug({packageJsonFilePath})
+  const packageName = options.packageName ?? await import(packageJsonFilePath, {assert: {type: 'json'}}).then(
     (pkg) => pkg.name
   );
+  debug({packageName})
 
   await npmPull({
     packageName,
