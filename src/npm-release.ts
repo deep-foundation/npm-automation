348d94ebe7ab40b131865c7f4d2be38818094e58
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-03 14:42:18 +0600

    Add debugs

--- src/npm-release.ts
+++ src/npm-release.ts
@@ -2,6 +2,7 @@ import exec from '@simplyhexagonal/exec';
 import { execAndLogStdoutOrThrowError } from './exec-and-log-stdout-or-throw-error.js';
 import { updateDeepJsonVersion } from './update-version-in-json-object.js';
 import { syncDependencies } from './sync-dependencies.js';
+import createDebugMessages from 'debug';
 
 /**
  * Releases a new version of the deep npm package and syncronizes the version and dependencies between {@link NpmReleaseParam.deepJsonFilePath} and {@link NpmReleaseParam.packageJsonFilePath}
@@ -19,32 +20,41 @@ await npmRelease({
  * 
  */
 export async function npmRelease(param: NpmReleaseParam) {
+  const debug = createDebugMessages(
+    '@deep-foundation/npm-automation:npm-release'
+  );
+  debug({ param });
   const {
     deepJsonFilePath,
     newVersion,
-    packageJsonFilePath: packageJsonPath,
+    packageJsonFilePath,
   } = param;
-  await syncDependencies({ deepJsonFilePath, packageJsonFilePath: packageJsonPath });
+  await syncDependencies({ deepJsonFilePath, packageJsonFilePath: packageJsonFilePath });
   
-  const packageJson = await import(packageJsonPath, {assert: {type: 'json'}});
+  const packageJson = await import(packageJsonFilePath, {assert: {type: 'json'}});
+  debug({packageJson})
   const { execPromise } = exec(`npm view ${packageJson.name} version`);
-  const execResult = await execPromise;
-  if (execResult.exitCode !== 0) {
-    throw new Error(execResult.stderrOutput.trim());
+  const npmViewExecResult = await execPromise;
+  debug({npmViewExecResult})
+  if (npmViewExecResult.exitCode !== 0) {
+    throw new Error(npmViewExecResult.stderrOutput.trim());
   }
-  const npmLatestPackageJsonVersion = execResult.stdoutOutput.toString().trim();
+  const npmLatestPackageJsonVersion = npmViewExecResult.stdoutOutput.toString().trim();
+  debug({npmLatestPackageJsonVersion})
   const packageJsonVersion = packageJson.version;
-  if (npmLatestPackageJsonVersion > packageJsonVersion) {
+  const isPackageJsonVersionOutdated = npmLatestPackageJsonVersion > packageJsonVersion;
+  debug({isPackageJsonVersionOutdated})
+  if (isPackageJsonVersionOutdated) {
     throw new Error(
-      `Version ${packageJson.version} in ${packageJsonPath} is outdated. Latest version in npm is ${npmLatestPackageJsonVersion}. Execute npm-pull`
+      `Version ${packageJson.version} in ${packageJsonFilePath} is outdated. Latest version in npm is ${npmLatestPackageJsonVersion}. Execute npm-pull`
     );
-  }
-  if (npmLatestPackageJsonVersion <= packageJsonVersion) {
+  } else {
     const {
       execResult: { stdoutOutput: npmVersionStdoutOutput },
     } = await execAndLogStdoutOrThrowError({
       command: `npm version --allow-same-version --no-git-tag-version ${newVersion}`,
     });
+    debug({npmVersionStdoutOutput})
     await updateDeepJsonVersion({
       version: npmVersionStdoutOutput.trimEnd().slice(1),
       filePath: deepJsonFilePath,
