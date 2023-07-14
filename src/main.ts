75cb932908656152ce0515f8c3b35d2171ccef48
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-14 23:14:10 +0600

    Add type keyword

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-export { npmInstall,NpmInstallParam } from './npm-install.js';
-export { syncDependencies,SyncDependenciesParam } from './sync-dependencies.js';
-export { npmRelease, NpmReleaseParam } from './npm-release.js';
-export { npmPull, NpmPullParam } from './npm-pull.js';
+export { npmInstall, type NpmInstallParam } from './npm-install.js';
+export { syncDependencies, type SyncDependenciesParam } from './sync-dependencies.js';
+export { npmRelease, type NpmReleaseParam } from './npm-release.js';
+export { npmPull, type NpmPullParam } from './npm-pull.js';
 export {generatePackageClass, type GeneratePackageClassParam} from './generate-package-class.js'
