8eed53d57e9a27f0b14958f11465a4fbd5771b5b
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-15 00:14:36 +0600

    Update generate package class tests

--- __tests__/generate-package-class.ts
+++ __tests__/generate-package-class.ts
@@ -1,15 +1,14 @@
-// Import the function and types from the module to be tested
 import { generatePackageClass, GeneratePackageClassParam } from '../src/generate-package-class';
-import fs from 'fs-extra'; // Import fs-extra for file operations
-import mockFs from 'mock-fs'; // Import mock-fs to mock the file system
+import fs from 'fs-extra';
+import mockFs from 'mock-fs';
 
-// Mock the 'fs-extra' module with 'mock-fs'
-jest.mock('fs-extra', () => require('mock-fs'));
+// Jest provides a clearAllMocks function that can clean up all the manual mocks
+afterEach(() => {
+  jest.clearAllMocks();
+  mockFs.restore();
+});
 
-// We use 'describe' to group our tests
 describe('generatePackageClass', () => {
-
-  // Before each test, we setup a mock file system with 'mockFs'
   beforeEach(() => {
     mockFs({
       'path/to/deepJson': JSON.stringify({
@@ -17,19 +16,13 @@ describe('generatePackageClass', () => {
           data: [
             { id: 'testId1' },
             { id: 'testId2' },
-            { id: 12345 }, // Notice this id is a number
+            { id: 12345 },
           ],
         },
       }),
     });
   });
 
-  // After each test, we clear the mock file system
-  afterEach(() => {
-    mockFs.restore();
-  });
-
-  // Test case: when the deep.json file does not exist
   it('should throw an error if the deep.json file does not exist', async () => {
     const params: GeneratePackageClassParam = {
       deepJsonFilePath: 'path/to/nonexistent/deepJson',
@@ -37,13 +30,11 @@ describe('generatePackageClass', () => {
       packageName: 'testPackage',
     };
 
-    // We expect an error to be thrown
     await expect(generatePackageClass(params)).rejects.toThrow(
       new Error(`deep.json file does not exist in ${params.deepJsonFilePath}`),
     );
   });
 
-  // Test case: when the deep.json file exists
   it('should generate a package class and write it to the output file', async () => {
     const params: GeneratePackageClassParam = {
       deepJsonFilePath: 'path/to/deepJson',
@@ -51,18 +42,12 @@ describe('generatePackageClass', () => {
       packageName: 'testPackage',
     };
 
-    // Run the function
     await generatePackageClass(params);
 
-    // Read the content of the output file
     const writtenClassDefinition = await fs.readFile(params.outputFilePath, 'utf-8');
-
-    // We expect the written class to contain certain strings
-    expect(writtenClassDefinition).toContain("public name: string = 'testPackage'"); // the package name
-    expect(writtenClassDefinition).toContain("public testId1 = this.createEntity(\"testId1\")"); // the first link id
-    expect(writtenClassDefinition).toContain("public testId2 = this.createEntity(\"testId2\")"); // the second link id
-
-    // We expect the written class NOT to contain the id which is a number
+    expect(writtenClassDefinition).toContain("public name: string = 'testPackage'");
+    expect(writtenClassDefinition).toContain("public testId1 = this.createEntity(\"testId1\")");
+    expect(writtenClassDefinition).toContain("public testId2 = this.createEntity(\"testId2\")");
     expect(writtenClassDefinition).not.toContain("public 12345 = this.createEntity(\"12345\")");
   });
 });
