4fb203e4be977931af2193c5c05e15f39fc9007a
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-14 23:13:55 +0600

    Export generatePackageClass

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,4 @@ export { npmInstall,NpmInstallParam } from './npm-install.js';
 export { syncDependencies,SyncDependenciesParam } from './sync-dependencies.js';
 export { npmRelease, NpmReleaseParam } from './npm-release.js';
 export { npmPull, NpmPullParam } from './npm-pull.js';
+export {generatePackageClass, type GeneratePackageClassParam} from './generate-package-class.js'
