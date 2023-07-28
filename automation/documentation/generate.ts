ba06841f538e2a15fc6e390f02f795524b50a392
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-28 10:52:30 +0600

    Chmod cli apps

--- automation/documentation/generate.ts
+++ automation/documentation/generate.ts
@@ -1,11 +1,15 @@
 import { glob } from 'glob';
 import {generateDocumentation} from '../../src/generate-documentation.js'
 import path from 'path';
+import fsExtra from 'fs-extra';
 
 main();
 
 async function main() {
   const cliAppFilePaths = await glob(`./dist/cli/*.js`, {absolute: true})
+  for (const cliAppFilePath of cliAppFilePaths) {
+    fsExtra.chmodSync(cliAppFilePath, '755');
+  }
   await generateDocumentation({
     generateCliAppsHelpInReadmeOptions: {
       cliAppFilePaths: cliAppFilePaths,
