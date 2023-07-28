311b66676e51335b92bb57ae8e9f49214bb917da
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-28 11:29:20 +0600

    Enable debugging

--- automation/documentation/generate.ts
+++ automation/documentation/generate.ts
@@ -7,7 +7,6 @@ import { execa } from 'execa';
 main();
 
 async function main() {
-  process.env.DEBUG = `*`;
   const cliAppFilePaths = await glob(`./dist/cli/*.js`, {absolute: true})
   for (const cliAppFilePath of cliAppFilePaths) {
     fsExtra.chmodSync(cliAppFilePath, '755');
