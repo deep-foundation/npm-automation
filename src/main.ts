e7334971984945b274399ec92138e3bf8bcd6408
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-28 10:09:48 +0600

    Add export

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,4 @@ export { npmRelease, type NpmReleaseOptions } from './npm-release.js';
 export { npmPull, type NpmPullOptions } from './npm-pull.js';
 export {generatePackageClass, type GeneratePackageClassOptions} from './generate-package-class.js'
 export {generateDocumentation, type GenerateDocumentationOptions} from './generate-documentation.js'
+export {buildTypescriptLibrary, type BuildTypescriptLibraryOptions} from './build-typescript-library.js'
